eac565b542a7b502a6ed9ccce3ec3439219a3a29
KingOmmin <darthommin@outlook.com>
2025-05-08 11:06:41 -0400

    new file:   .gitignore 	new file:   Object/.classpath 	new file:   Object/.gitignore 	new file:   Object/.project 	new file:   Object/.settings/org.eclipse.core.resources.prefs 	new file:   Object/.settings/org.eclipse.jdt.core.prefs 	new file:   Object/src/a/Circle.java 	new file:   Object/src/a/Main.java 	new file:   Object/src/module-info.java 	new file:   first/.classpath 	new file:   first/.gitignore 	new file:   first/.project 	new file:   first/.settings/org.eclipse.core.resources.prefs 	new file:   first/.settings/org.eclipse.jdt.core.prefs 	new file:   first/src/module-info.java

---
+++ Object/src/a/Circle.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package a;
+
+public class Circle {
+	
+	public double radius;
+	public String color;
+	
+	Circle(){
+		radius = 1.0;
+		color = "blue";
+	}//default constructor
+	Circle(double rad, String col){
+		radius = rad;
+		color = col;
+	}
+	
+	public double getRadius() {
+		return radius;
+	}
+	public void setRadius(double radius) {
+		this.radius = radius;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public double calcArea() {
+		return Math.PI* this.radius*this.radius;
+	}//end method
+	public double calcCircumference() {
+		return 2* Math.PI* this.radius;
+	}//end method
+}
---
+++ Object/src/a/Main.java
@@ -0,0 +1,12 @@
+package a;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Circle cir1 = new Circle(3.2,"green");
+
+		
+	}
+
+}
